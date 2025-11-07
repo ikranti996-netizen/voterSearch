12e4c39702b093ea5885e57571f3ef48e33635f9
ikranti996-netizen <ikranti996@gmail.com>
2025-11-07 01:37:29 -0800

    nes changes

--- voter-search/src/App.jsx
+++ voter-search/src/App.jsx
@@ -828,6 +828,16 @@ export default function App() {
               <span style={heart} aria-hidden="true">
                 ❤️
               </span>
+              <br />
+              <span
+                style={{
+                  display: "inline-block",
+                  marginLeft: 6,
+                  fontWeight: 600,
+                }}
+              >
+                राजा वही बनेगा जो हकदार होगा
+              </span>
             </div>
           </div>
           <div
@@ -879,6 +889,33 @@ export default function App() {
             <span style={tinyHeart} aria-hidden="true">
               ❤️
             </span>
+          </div>
+          <div
+            id="post-footer-banner"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: 12,
+              padding: "0 20px",
+            }}
+          >
+            <img
+              src={bannerUrl1}
+              alt="Footer banner"
+              loading="lazy"
+              crossOrigin="anonymous"
+              onError={(e) => {
+                e.currentTarget.src = bannerUrl;
+              }}
+              style={{
+                width: "100%",
+                maxWidth: 1100,
+                height: "auto",
+                borderRadius: 12,
+                boxShadow: "0 12px 36px rgba(2,6,23,0.06)",
+                display: "block",
+              }}
+            />
           </div>
         </footer>
 
